a943f671b5e24fbaaf627e4d57759b435a7a095b
Tim Beck <tim.beck46@gmail.com>
2019-04-01 19:50:19 -0400

    Add Likes model, associate Users and Events with them

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   # :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :likes
+  has_many :events, through: :likes
 end
