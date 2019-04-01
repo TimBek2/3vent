e03f838668b9ab748bee051ce253715caad7bd4b
Tim Beck <tim.beck46@gmail.com>
2019-03-31 20:22:39 -0400

    Create Event Model, add Event CRUD behavior

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :views
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :events
+
   root to: 'static_pages#home'
 end
